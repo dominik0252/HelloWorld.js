83d41eef070f5cabdabd686f7a82dd1a0a89c687
Dominik Vidusin <dominik.vidusin@gmail.com>
2020-10-22 18:15:26 +0200

    response  object

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ app.use('/', (req, res) => {              // specifying that the URI '/' will tr
   var url = req.url;        // the resource that is being requested
   var agent = req.headers['user-agent'];
   agent = req.get('User-Agent'); // generally recommended to use the get function to request a specific header field, one of the reasons is that it's case insensitive
-  
-  res.send('Hello World!');
+
+  res.status(200);  // set the status
+  res.type('html'); // set the response type
+  res.write('Hello world!');  // the response is a stream and we can continually write to that stream
+  res.write('<p>');
+  res.write('<b>Have a nice day</b>');
+  res.end();  // this will send the HTTP response and close the connection
+
+  // res.send('Hello World!');
 });
 
 app.listen(3000, () => {                  // configure the app so that it's listening on the port 3000
