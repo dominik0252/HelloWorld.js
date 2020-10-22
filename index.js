40576dc64039edc7385afd53d9c0a1984fc981d3
Dominik Vidusin <dominik.vidusin@gmail.com>
2020-10-22 18:23:36 +0200

    combining requests and responses

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ var app = express();                      // to create an express app
 // we configure our app by setting up an event handler or a callback function
 app.use('/', (req, res) => {              // specifying that the URI '/' will trigger an event
                                           // the event is invoke the callback function that takes the request object and the response object
-  var method = req.method;  // HTTP verb
-  var url = req.url;        // the resource that is being requested
-  var agent = req.headers['user-agent'];
-  agent = req.get('User-Agent'); // generally recommended to use the get function to request a specific header field, one of the reasons is that it's case insensitive
+  // var method = req.method;  // HTTP verb
+  // var url = req.url;        // the resource that is being requested
+  // var agent = req.headers['user-agent'];
+  // agent = req.get('User-Agent'); // generally recommended to use the get function to request a specific header field, one of the reasons is that it's case insensitive
 
-  res.status(200);  // set the status
-  res.type('html'); // set the response type
-  res.write('Hello world!');  // the response is a stream and we can continually write to that stream
-  res.write('<p>');
-  res.write('<b>Have a nice day</b>');
-  res.end();  // this will send the HTTP response and close the connection
+  // res.status(200);  // set the status
+  // res.type('html'); // set the response type
+  // res.write('Hello world!');  // the response is a stream and we can continually write to that stream
+  // res.write('<p>');
+  // res.write('<b>Have a nice day</b>');
+  // res.end();  // this will send the HTTP response and close the connection
+
+  var name = req.query.name;
+
+  res.status(200).type('html');
+
+  if(name) {
+    res.write('Hi ' + name + ", it's nice to see you.");
+  } else {
+    res.write('Welcome, guest!');
+  }
+
+  res.end();
 
   // res.send('Hello World!');
 });
